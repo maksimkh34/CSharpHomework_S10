900cc3c290968274eb9a50dc595d308d17fe621b
Maksim <you@epic.twod@mail.ru>
2022-07-23 17:01:19 +0300

    Task 1

---
+++ Z1/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] array = {"q", "a", "w", "dgs"};
+
+int countstrs(string[] texts)
+{
+    int nums = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        switch(texts[i][0])
+        {
+            case 'a':
+            nums++;
+            break;
+            case 'e':
+            nums++;
+            break;
+            case 'i':
+            nums++;
+            break;
+            case 'o':
+            nums++;
+            break;
+            case 'u':
+            nums++;
+            break;
+            case 'y':
+            nums++;
+            break;
+        }
+
+    }
+    return nums;
+}
+
+Console.WriteLine(countstrs(array));
